02a64110fdb9843fd3d9fb11b57753bd2cbc573d
Lidia <lilkalilson@gmail.com>
2024-12-10 17:29:47 +0100

    add task1

---
+++ 00.py
@@ -0,0 +1,5 @@
+# 1. Po wpisaniu nazwy potrawy, chcę widzieć listę składników potrzebnych do przygotowania danego posiłku.
+# przykład:
+# > Wybierz potrawę (spaghetti, pomidorówka, jajecznica)
+# > spaghetti
+# > Potrzebujesz: makaron, sos, mięso
