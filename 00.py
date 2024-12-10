f21c07b8ac638cecc19aed21831f6caed3d904bb
Lidia <lilkalilson@gmail.com>
2024-12-10 18:47:58 +0100

    part one

--- 00.py
+++ 00.py
@@ -3,3 +3,25 @@
 # > Wybierz potrawę (spaghetti, pomidorówka, jajecznica)
 # > spaghetti
 # > Potrzebujesz: makaron, sos, mięso
+
+
+
+# wybor_potrawy = str(input("Podaj nazwę potrawy, a wyświetli się lista potrzebnych składników: "))
+# print(wybor_potrawy)
+potrawy = {"spaghetti": ["makaron", "mięso", "sos"], "pomidorówka": ["makaron", "rosół_z_wczoraj"]}
+
+potrawa = input("Podaj nazwę potrawy: ")
+spaghetti = potrawy[potrawa]
+print(spaghetti)
+
+
+
+# def moja_funkcja():
+#     return 5
+ 
+# wynik = moja_funkcja()
+# print(wynik)
+
+# lista = [a, b, c]
+# krotka = (a, b ,c)
+# słownik = {a: a1, b: b2, c: c3}
