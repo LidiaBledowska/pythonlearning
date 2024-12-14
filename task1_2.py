6c80fbd11587ccdac1e34d231148d68c06bbd607
Lidia <lilkalilson@gmail.com>
2024-12-14 19:39:41 +0100

    new user program to delete meal

--- task1_2.py
+++ task1_2.py
@@ -8,7 +8,7 @@ from fun import is_key_exist
 meals_2 = {"spaghetti": ["makaron", "mięso", "sos"], "pomidorówka": ["makaron", "rosół_z_wczoraj"], "jajecznica": ["jajka", "masło"], "żurek": ["żur", "jajka", "kiełbasa"]} 
 variable = True 
 while variable == True:
-    choice = input('Napisz:\n 1 jeśli chcesz wybrać potrawę wraz ze składnikami, \n 2 jeśli chcesz dodać swoją potrawę wraz ze składnikami, \n 3 jeśli program ma zakończyć działanie, \n 4 jeśli chcesz zmienić potrawy w słowniku \n: ')
+    choice = input('Napisz:\n 1 jeśli chcesz wybrać potrawę wraz ze składnikami, \n 2 jeśli chcesz dodać swoją potrawę wraz ze składnikami, \n 3 jeśli program ma zakończyć działanie, \n 4 jeśli chcesz zmienić potrawy w słowniku, \n 5 jeśli chcesz usunąć potrawę ze słownika, \n 6 jeśli chcesz zobaczyć listę potraw po wprowadzonych zmianach \n: ')
     if choice == '1':
         show_ingredients(meals=meals_2)
     elif choice == '2':
@@ -27,6 +27,11 @@ while variable == True:
             meals_2[name_change_meal] = user_ingredients
         else:
             print("Nie ma takiej potrawy.")
+    elif choice == '5':
+        print(meals_2)
+        name_meal = input("Podaj nazwę potrawy, która ma zostać usunięta: ")
+        del meals_2[name_meal]
+    # elif choice == '6':
     elif choice == '3':
         variable = False
         print("Koniec programu")
